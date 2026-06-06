dab793e2566dc7813e1297c13d5a0b4771abd47d
Thomas Schrefl <thomas.schrefl@donau-uni.ac.at>
2026-06-06 22:29:03 +0200

    chore: clean up root directory and move sphere samples to samples/
    - Moved sphere_40nm model files (.npz, .krn, .p2) and run_sphere.sh to samples/ directory.
    - Updated run_sphere.sh to work from its new location.
    - Deleted all temporary test artifacts and local results from the root directory.

---
+++ samples/run_sphere.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# run_sphere.sh
+# Run this from the samples/ directory
+set -e
+
+# Run the simulation
+pixi run python ../src/loop.py sphere_40nm --add-shell --layers 4 --out-dir hyst_sphere_40nm --verbose
+
+# Plot the results
+pixi run python ../src/plot_hysteresis.py hyst_sphere_40nm/hysteresis.csv --out sphere_hyst.png
