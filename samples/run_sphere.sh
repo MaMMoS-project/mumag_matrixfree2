1a9792bd3ed2854c04f45f67573960f705482ea0
Thomas Schrefl <thomas.schrefl@donau-uni.ac.at>
2026-06-07 03:49:28 +0200

    correct run_sphere.sh for plotting, set fast convergence for in p2

--- samples/run_sphere.sh
+++ samples/run_sphere.sh
@@ -7,4 +7,4 @@ set -e
 pixi run python ../src/loop.py sphere_40nm --add-shell --layers 4 --out-dir hyst_sphere_40nm --verbose
 
 # Plot the results
-pixi run python ../src/plot_hysteresis.py hyst_sphere_40nm/hysteresis.csv --out sphere_hyst.png
+pixi run python ../src/plot_hysteresis.py hyst_sphere_40nm/hysteresis.csv hyst_sphere_40nm/sphere_hyst.png
