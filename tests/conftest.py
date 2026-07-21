c5217e1720a494d8c453ba193374c225936ef6aa
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-07-21 13:00:35 +0200

    add stable mesh tests

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+"""Test configuration."""
+
+import sys
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def mesh_bin():
+    """Define `mesh` bin."""
+    return f"{sys.executable} {Path(__file__).resolve().parent.parent / 'src' / 'mesh.py'}"
