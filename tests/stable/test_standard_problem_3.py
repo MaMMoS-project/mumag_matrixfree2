9ad27836c7174a1a440e568f3030c8ca62d41629
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-07-23 14:56:43 +0200

    work in progress (not working yet)

--- tests/stable/test_standard_problem_3.py
+++ tests/stable/test_standard_problem_3.py
@@ -1,5 +1,6 @@
 """Test standard problem 3."""
 
+from dataclasses import replace
 import sys
 from pathlib import Path
 
@@ -14,6 +15,7 @@ import mesh
 from energy_kernels import make_energy_kernels
 from fem_utils import TetGeom, compute_node_volumes
 from loop import compute_grad_phi_from_JinvT, compute_volume_JinvT, load_materials_krn, load_params_p2
+from poisson_solve import make_solve_U
 
 jax.config.update("jax_enable_x64", True)
 
@@ -25,45 +27,18 @@ def test_standard_problem_3(loop_bin, mesh_bin, tmp_path):
     - https://ubermag.github.io/examples/notebooks/07-tutorial-standard-problem3.html
     """
     _write_p2_file(tmp_path / "std3.p2")
-    lex = _write_krn_file(tmp_path / "std3.krn")
-    params = load_params_p2(tmp_path / "std3.p2")
-    A, K1, Js, k_easy = load_materials_krn(
-        tmp_path / "std3.krn",
-        G=1,  # magnetic regions
-        mesh_unit=params["mesh_unit"],
-    )
+    _write_krn_file(tmp_path / "std3.krn")
 
     energy_diff = []
     L_array = np.linspace(8, 9, 5)
     h = 0.2
     for L in L_array:
         print(f"{L=}")
-        knt, geom, vol, mat_id = _get_mesh(L * lex, h * lex, tmp_path)
-        m_vortex = _get_vortex(knt)
-        m_flower = _get_flower(knt)
-        V_mag_nm = np.sum(vol[mat_id == 1])
-        vol_Js = vol * np.array(Js[mat_id - 1])
-        M_nodal = compute_node_volumes(
-            TetGeom(conn=geom.conn, volume=jnp.asarray(vol_Js), mat_id=geom.mat_id),
-            chunk_elems=200_000,
-        )
-        energy_and_grad, *_ = make_energy_kernels(
-            geom,
-            A,
-            K1,
-            Js,
-            k_easy,
-            float(V_mag_nm),
-            M_nodal,
-            chunk_elems=200_000,
-        )
-        U_vortex = solve_U(m_vortex, jnp.zeros(m_vortex.shape[0]))
-        E_vortex, _ = energy_and_grad(m_vortex, U_vortex, jnp.zeros(3))
+        E_vortex = _get_energy(L, h, "vortex", tmp_path)
         print(f"{E_vortex=}")
-        U_vortex = solve_U(m_flower, jnp.zeros(m_flower.shape[0]))
-        E_flower, _ = energy_and_grad(m_flower, U_flower, jnp.zeros(3))
+        E_flower = _get_energy(L, h, "flower", tmp_path)
         print(f"{E_flower=}")
-        energy_diff.appendd(E_vortex - E_flower)
+        energy_diff.append(E_vortex - E_flower)
 
     crossing = _evaluate_crossing(L_array, energy_diff)
     assert np.isclose(crossing, 8.5)
@@ -75,7 +50,6 @@ def _write_krn_file(filename):
     A = 7.7e-12
     Km = 0.5 * Js * Js / mu_0
     K1 = 0.1 * Km
-    lex = np.sqrt(A / Km)
     krn_file = Path(filename)
     krn_file.write_text(
         f"""\
@@ -84,7 +58,75 @@ def _write_krn_file(filename):
 \
         """
     )
-    return lex
+
+
+def _write_p2_file(filename):
+    p2_file = Path(filename)
+    p2_file.write_text(
+        """\
+[mesh]
+size = 1e-9
+\
+        """
+    )
+
+
+def _get_energy(L, h, init_mag, tmp_path):
+    params = load_params_p2(tmp_path / "std3.p2")
+
+    # materials
+    mu_0 = 4e-7 * np.pi
+    Js = 1.6
+    A = 7.7e-12
+    Km = 0.5 * Js * Js / mu_0
+    K1 = 0.1 * Km
+    k_easy = np.array([0.0, 0.0, 1.0])
+    lex = np.sqrt(A / Km)
+
+    # materials rescaling
+    Kd = (Js * Js) / (2.0 * mu_0)
+    A_red = A / (Kd * params["mesh_unit"] ** 2)
+    K1_red = K1 / Kd
+    Js_red = 1.0
+    Js_lookup = jnp.array([Js_red, 0.0])
+    K1_lookup = jnp.array([K1_red, 0.0])
+    A_lookup = jnp.array([A_red, 0.0])
+    k_easy_lookup = jnp.array([k_easy, k_easy])
+
+    # mesh
+    cube_length = L * lex / params["mesh_unit"]
+    mesh_size = h * lex / params["mesh_unit"]
+    knt, geom, volume, mat_id, boundary_mask = _get_mesh(cube_length, mesh_size, tmp_path)
+
+    # initial magnetization
+    match init_mag:
+        case "vortex":
+            m = _get_vortex(knt)
+        case "flower":
+            return 0  # TEMP
+            m = _get_flower(knt)
+        case _:
+            raise ValueError
+
+    # magnetic volume and other stuff
+    V_mag_nm = np.sum(volume[mat_id == 1])
+    vol_Js = volume * np.array(Js_lookup[mat_id - 1])
+    geom_Js = replace(geom, volume=jnp.asarray(vol_Js))
+    M_nodal = compute_node_volumes(geom_Js, chunk_elems=200_000)
+    energy_and_grad, *_ = make_energy_kernels(
+        geom,
+        A_lookup,
+        K1_lookup,
+        Js_lookup,
+        k_easy_lookup,
+        float(V_mag_nm),
+        M_nodal,
+        chunk_elems=200_000,
+    )
+    solve_U = make_solve_U(geom, Js_lookup, boundary_mask=boundary_mask)
+    U = solve_U(m, jnp.zeros(m.shape[0]))
+    E, _ = energy_and_grad(m, U, jnp.zeros(3))
+    return E
 
 
 def _get_vortex(knt):
@@ -104,8 +146,9 @@ def _get_flower(knt):
 
 
 def _get_mesh(L, h, tmp_path):
+    print(f"Cube size: {L}. Mesh size: {h}")
     mesh_filename = str(tmp_path / "std3.npz")
-    knt, ijk, *_ = mesh.run_single_solid_mesher(
+    mesh.run_single_solid_mesher(
         geom="box",
         extent=f"{L},{L},{L}",
         h=h,
@@ -134,26 +177,16 @@ def _get_mesh(L, h, tmp_path):
         mat_id=jnp.asarray(mat_id, dtype=jnp.int32),
         grad_phi=jnp.asarray(grad_phi, dtype=jnp.float64),
     )
-    return knt, geom, volume, mat_id
+    return knt, geom, volume, mat_id, boundary_mask
 
 
 def _evaluate_crossing(L, energy_diff):
     return
 
 
-def _write_p2_file(filename):
-    p2_file = Path(filename)
-    p2_file.write_text(
-        """\
-[mesh]
-size = 1e-9
-\
-        """
-    )
-
-
 if __name__ == "__main__":
     loop_bin = f"{sys.executable} {Path(__file__).resolve().parent.parent / 'src' / 'loop.py'}"
     mesh_bin = f"{sys.executable} {Path(__file__).resolve().parent.parent / 'src' / 'mesh.py'}"
-    tmp_path = Path(__file__).parent.parent.parent / "tmp"
-    test_standard_problem_3(loop_bin, mesh_bin, tmp_path)
+    tmp_dir = Path(__file__).parent.parent.parent / "tmp"
+    tmp_dir.mkdir(exist_ok=True)
+    test_standard_problem_3(loop_bin, mesh_bin, tmp_dir)
