22a22594062fc4fd4c1c408e75ed5b093d66a764
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2026-08-06 11:15:21 +0200

    reorder imports

--- src/tommos/minimizers.py
+++ src/tommos/minimizers.py
@@ -21,6 +21,7 @@ Advanced micromagnetic energy minimizers:
 
 from __future__ import annotations
 
+import os
 import time
 from collections.abc import Callable
 from dataclasses import dataclass
--- src/tommos/poisson_solve.py
+++ src/tommos/poisson_solve.py
@@ -8,6 +8,7 @@ License: MIT
 
 from __future__ import annotations
 
+import os
 from collections.abc import Callable
 from functools import partial
 from typing import Any, Literal
